c7d557dc532718feac2c6e02b304cec4d394f524
Piotr Wojcieszuk <pwojcieszuk@gmail.com>
2023-12-14 21:38:25 +0100

    Enable validation

--- src/calculation/dto/calculation.dto.ts
+++ src/calculation/dto/calculation.dto.ts
@@ -1,9 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class CalculationDto {
   @ApiProperty({
     description: 'The expression to be calculated',
     example: '2 + 2',
+  })
+  @IsNotEmpty({ message: 'Expression is required' })
+  @IsString({ message: 'Expression must be a string' })
+  @Matches(/^[0-9+\-*/(). ]+$/, {
+    message: 'Expression contains invalid characters',
   })
   expression: string;
 }
